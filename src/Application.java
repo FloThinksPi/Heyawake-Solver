f36b0af59766fbaf256045c8ddd29e43aa77b6bb
FloThinksPi <flo@myprivatemail.de>
2016-02-15 01:21:18 +0100

    Recursive Result Calculation Finished TODO: WhiteLines Algorythm

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by flo on 10.02.16.
@@ -21,20 +18,21 @@ public class Application {
 
         int[][][][] roomPossibilityMatrix = calculateAllPossibilitysForRooms(roomInfoMatrix, roomMatrix);
 
+        //TODO Sort Possibilitys to get better performance
 
-        int[][] resultMatrix = new int[blackMatrix.length][blackMatrix[0].length];
+        int[][] resultMatrix = new int[roomMatrix.length][roomMatrix[0].length];
+        resultMatrix = findResult(roomPossibilityMatrix, roomMatrix, resultMatrix, 0);
 
-        for (int row = 0; row < blackMatrix.length; row++) {
-            for (int cell = 0; cell < blackMatrix[0].length; cell++) {
-                resultMatrix[row][cell] = roomPossibilityMatrix[roomMatrix[row][cell]][0][row][cell];
+        if (resultMatrix != null) {
+            System.out.println("\nFINAL Solution for Room ");
+            for (int[] ints : resultMatrix) {
+                System.out.println(Arrays.toString(ints));
             }
+            System.out.println("\n");
+        } else {
+            System.out.println("Keine Lösung gefunden!");
         }
 
-        System.out.println("\nFINAL Solution for Room ");
-        for (int[] ints : resultMatrix) {
-            System.out.println(Arrays.toString(ints));
-        }
-        System.out.println("\n");
 
     }
 
@@ -111,7 +109,7 @@ public class Application {
         for (int row = itterationRow; row < roomInfoMatrix[roomIndex][2]; row++) {
             for (int cell = startCell; cell < roomInfoMatrix[roomIndex][1]; cell++) {
 
-                if (checkBlackNeighbours(tempRoom, row, cell)) {
+                if (checkBlackNeighbours(tempRoom, row, cell,true)) {
                     tempRoom[row][cell] = 1;
                     gesetzt++;
                 }
@@ -132,24 +130,13 @@ public class Application {
             }
         }
 
-        //TODO Erweitere Rooms auf volle Rätsel größe
-
         return possibilityResults;
     }
 
-    public static int[][] solve(int[][] blackMatrix, int[] countableRoomMatrix, int[][] blackCountMatrix, int[][] roomMatrix, int stepRow, int stepCell) {
-
-
-        //TODO Try out all Permutations of Rooms
-
-
-        return blackMatrix;
-    }
-
     //Low CPU Time (9 Compares Worscase=Commen Use)
-    public static boolean checkBlackNeighbours(int[][] blackMatrix, int row, int cell) {
+    public static boolean checkBlackNeighbours(int[][] blackMatrix, int row, int cell,boolean includeOwnField) {
 
-        if (blackMatrix[row][cell] == 1) return false;
+        if (includeOwnField&&blackMatrix[row][cell] == 1) return false;
 
         if (row != 0) {
             if (blackMatrix[row - 1][cell] == 1) return false;
@@ -170,6 +157,58 @@ public class Application {
         return true;
     }
 
+    public static int[][] findResult(int[][][][] roomPossibilityMatrix, int[][] roomMatrix, int[][] resultMatrix, int startRoomIndex)  {
+
+        int[][] tempResultMatrix = resultMatrix;
+        boolean checkNeigbours=true;
+        boolean checkWhiteReachable=true;
+
+        for (int possibility = 0; possibility < roomPossibilityMatrix[startRoomIndex].length; possibility++) {
+//            System.out.println("Room: "+startRoomIndex+" Possibillity: "+possibility);
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            //Todo start with OFFSET (Width and Height) -> Better Performance
+            for (int row = 0; row < roomMatrix.length; row++) {
+                for (int cell = 0; cell < roomMatrix[0].length; cell++) {
+                    if(roomMatrix[row][cell]==startRoomIndex)tempResultMatrix[row][cell] = roomPossibilityMatrix[startRoomIndex][possibility][row][cell];
+                }
+            }
+
+            for (int row = 0; row < roomMatrix.length; row++) {
+                for (int cell = 0; cell < roomMatrix[0].length; cell++) {
+
+                    if (tempResultMatrix[row][cell]==1&&!checkBlackNeighbours(tempResultMatrix, row, cell,false)){
+                        checkNeigbours=false;
+                        break;
+                    }
+
+                }
+                if(checkNeigbours==false)break;
+            }
+            if(checkNeigbours==false)continue;
+
+            if (startRoomIndex != roomPossibilityMatrix.length - 1) {
+                tempResultMatrix = findResult(roomPossibilityMatrix, copyMatrix(roomMatrix), tempResultMatrix, startRoomIndex + 1);
+                if (tempResultMatrix != null) return tempResultMatrix;
+                else tempResultMatrix = resultMatrix;
+            } else {
+
+                if (checkNeigbours&&checkWhiteReachable(tempResultMatrix, 0, 0)) {
+                    return tempResultMatrix;
+                } else {
+                    checkWhiteReachable=false;
+                }
+
+            }
+
+        }
+
+        return null;
+
+    }
 
     //High CPU Time(Guessed)
     //Kann erst am ende bestimmt werden !
@@ -178,10 +217,58 @@ public class Application {
         return false;
     }
 
+
+    public static boolean checkWhiteReachable(int[][] blackMatrix, int startRow, int startCell) {
+
+        int[][] trackedMatrix = checkWhiteReachableRecursive(blackMatrix, 0, 0);
+
+//        System.out.println("\nWhite Pathfinding Matrix");
+        for (int[] ints : trackedMatrix) {
+            for (int anInt : ints) {
+                if (anInt == 0) return false;
+            }
+//            System.out.println(Arrays.toString(ints));
+        }
+//        System.out.println("\n");
+
+        return true;
+    }
+
     //Low CPU Time(Guessed)
-    public static boolean checkWhiteReachable(int[][] blackMatrix, int row, int cell) {
+    public static int[][] checkWhiteReachableRecursive(int[][] sourceMatrix, int startRow, int startCell) {
         //TODO Write Check for Connected Whites
-        return false;
+
+        int[][] blackMatrix = copyMatrix(sourceMatrix);
+
+        for (int row = 0; row < blackMatrix.length; row++) {
+            for (int cell = 0; cell < blackMatrix[0].length; cell++) {
+                if (blackMatrix[row][cell] == 0) {
+                    blackMatrix[row][cell] = 2;
+
+                    if (row != 0) {
+                        if (blackMatrix[row - 1][cell] == 0)
+                            blackMatrix = checkWhiteReachableRecursive(blackMatrix, (row - 1), cell);
+                    }
+
+                    if (row != blackMatrix.length - 1) {
+                        if (blackMatrix[row + 1][cell] == 0)
+                            blackMatrix = checkWhiteReachableRecursive(blackMatrix, (row + 1), cell);
+                    }
+
+                    if (cell != 0) {
+                        if (blackMatrix[row][cell - 1] == 0)
+                            blackMatrix = checkWhiteReachableRecursive(blackMatrix, row, (cell - 1));
+                    }
+
+                    if (cell != blackMatrix[0].length - 1) {
+                        if (blackMatrix[row][cell + 1] == 0)
+                            blackMatrix = checkWhiteReachableRecursive(blackMatrix, row, (cell + 1));
+                    }
+                }
+            }
+        }
+
+        return blackMatrix;
     }
 
     //Copys Array and inserts it in the right place in a Full sized(sized like complete heyawake) array.
@@ -204,6 +291,17 @@ public class Application {
         }
 
         return newMatrix;
+    }
+
+    public static int[][] copyMatrix(int[][] matrix) {
+        int[][] myInt = new int[matrix.length][];
+        for (int i = 0; i < matrix.length; i++) {
+            int[] aMatrix = matrix[i];
+            int aLength = aMatrix.length;
+            myInt[i] = new int[aLength];
+            System.arraycopy(aMatrix, 0, myInt[i], 0, aLength);
+        }
+        return myInt;
     }
 
 }
